ffda332c41d5fde406e8b80b76ea3f53a07c9121
Yu Ho Kwok <will.kwok@invivointeractive.com>
2021-02-23 10:59:07 +0800

    First Commit

---
+++ app/src/main/java/edu/shape/aad/MainActivity.java
@@ -0,0 +1,28 @@
+package edu.shape.aad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onDisplayClicked(View v) {
+        Log.i("Sample1 App", "Hello Logcat");
+        et = findViewById(R.id.editText);
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("Hello " + et.getText().toString());
+    }
+}
